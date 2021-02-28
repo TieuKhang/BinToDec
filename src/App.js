019a289f0798983d81f3d93d7a859b866bbcae04
TieuKhang <52084792+TieuKhang@users.noreply.github.com>
2021-02-27 21:22:07 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [binval, setBinVal] = useState('');
+  const [decRes, setDecRes] = useState('');
+  const [decval,setDecVal] = useState('');
+  const [binRes,setBinRes] = useState('');
+
+  const BinToDec = (e) => {
+    e.preventDefault();
+    var i=0;
+    var res=0;
+    var tempBin=binval;
+    while(tempBin>0){
+      res = res+ (tempBin % 10) * (2 ** i);
+      tempBin = (tempBin - tempBin%10)/10;
+      ++i;
+    }
+    console.log(res);
+    setDecRes(res);
+  }
+
+  const DecToBin = (e) => {
+    e.preventDefault();
+    var i=0;
+    var res=0;
+    var tempDec=decval;
+    while(tempDec>0){
+      res = res+ (tempDec % 2) * (10 ** i);
+      tempDec = (tempDec - tempDec%2)/2;
+      ++i;
+    }
+    console.log(res);
+    setBinRes(res);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Binary to Decimal converter</h1>
+      <form onSubmit={BinToDec}>
+        <input value = {binval} onChange={(e) => setBinVal(e.target.value)} placeholder="input a binary number"/>
+        <button type="submit"> Submit</button>
+      </form>
+      <p>The decimal value of this number is: {decRes}</p>
+
+      <h1>Decimal to Binary converter</h1>
+      <form onSubmit={DecToBin}>
+        <input value = {decval} onChange={(e) => setDecVal(e.target.value)} placeholder="input a decimal number"/>
+        <button type="submit"> Submit</button>
+      </form>
+      <p>The binary value of this number is: {binRes}</p>
     </div>
   );
 }
